4190796d9e65a54ac3d4d16244078539289f0808
Chigira <52981047+DaisukeChigira@users.noreply.github.com>
2019-08-19 00:42:35 +0900

    modabc136

--- ABC136/contest126B.py
+++ ABC136/contest126B.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #coding: utf-8
 n = int(input())
 ans = 0
@@ -6,12 +5,3 @@ for i in range(1,n+1):
     if len(str(i)) % 2 != 0:
         ans = ans + 1
 print(ans) 
-=======
-#coding: utf-8
-n = int(input())
-ans = 0
-for i in range(1,n+1):
-    if len(str(i)) % 2 != 0:
-        ans = ans + 1
-print(ans) 
->>>>>>> d1ea02c9f55043aea2e406cd5688608689b0edfc
--- ABC136/contest126C.py
+++ ABC136/contest126C.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #coding: utf-8
 n = int(input())
 boxes = list(map(int,input().split()))
@@ -13,19 +12,3 @@ for i in reversed(range(1,len(boxes))):
 		boxes[i-1] = boxes[i-1] -1
 
 print(answer) 
-=======
-#coding: utf-8
-n = int(input())
-boxes = list(map(int,input().split()))
-answer = 'Yes'
-
-for i in reversed(range(1,len(boxes))):
-	# print(str(i-1)+str(i))
-	if boxes[i-1] - boxes[i] > 1:
-		answer = 'No'
-		break
-	elif boxes[i-1] - boxes[i] == 1:
-		boxes[i-1] = boxes[i-1] -1
-
-print(answer) 
->>>>>>> d1ea02c9f55043aea2e406cd5688608689b0edfc
